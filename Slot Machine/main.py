38eeab44e68c1c0e6964ac219ff7d2d543749968
Akash Vijayakumar Rekha <141734126+akashvijayakumarrekha@users.noreply.github.com>
2023-08-25 16:19:21 +0200

    Winnings Check

--- Slot Machine/main.py
+++ Slot Machine/main.py
@@ -10,6 +10,24 @@ COLS = 3
 
 symbol_count = {"A": 2, "B": 4, "C": 6, "D": 8}
 
+symbol_value = {"A": 5, "B": 4, "C": 3, "D": 2}
+
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winnings_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winnings_lines.append(line + 1)
+
+    return winnings, winnings_lines
+
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
@@ -92,8 +110,7 @@ def get_bet():
     return amount
 
 
-def main():
-    balance = deposit()
+def spin(balance):
     line = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -110,6 +127,23 @@ def main():
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
 
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, line, bet, symbol_value)
+    print(f"You won €{winnings}.")
+    print(f"You won on lines:", *winning_lines)
+
+    return winnings - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is €{balance}")
+        answer = input("Press Enter to play (q to quit).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You are left with €{balance}")
 
 
 main()
