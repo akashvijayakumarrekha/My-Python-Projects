e17e10851791eb2c60e8b6dd330bebeeb679e037
Akash Vijayakumar Rekha <141734126+akashvijayakumarrekha@users.noreply.github.com>
2023-08-25 15:51:09 +0200

    Slot Machine

--- Slot Machine/main.py
+++ Slot Machine/main.py
@@ -13,7 +13,7 @@ symbol_count = {"A": 2, "B": 4, "C": 6, "D": 8}
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
-    for symbol, symbols_count in symbols.items():
+    for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
@@ -36,9 +36,10 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row], "|")
+                print(column[row], end=" | ")
             else:
-                print(column[row], "|")
+                print(column[row], end=" | ")
+        print()
 
 
 def deposit():
@@ -105,6 +106,10 @@ def main():
             break
 
     print(f"Your are betting €{bet} on {line} lines. Total bet is: €{total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+
+    print_slot_machine(slots)
 
 
 main()
